186025e1f886991023055b7e6c1aa44f9c41d6a3
mosaicmuse <93966161+mosaicmuse@users.noreply.github.com>
2022-09-11 21:45:57 -0700

    Replacing fields from API data

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 function displayTemperature(response) {
-  console.log(response.data.current.temp);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let conditionsElement = document.querySelector("#conditions");
+  temperatureElement.innerHTML = Math.round(response.data.current.temp);
+  cityElement.innerHTML = response.data.timezone;
+  conditionsElement.innerHTML = response.data.current.weather[0].description;
 }
 
 let apiKey = "5630ddfbed4d1b38c51e1715cf9e9557";
